ae6ed38415809c0f5ebefa83257451a704ce499a
thasanee <meow3525@gmail.com>
2022-01-03 16:39:00 -0600

    sum all number in a rangt

--- Intermediate-Algorithm-Scripting/diff-two-arr.js
+++ Intermediate-Algorithm-Scripting/diff-two-arr.js
@@ -1,7 +1,6 @@
 /* ============================== INSTRUCTIONS ==============================
  ================================================================================ */
 
-
 /* 
 Diff Two Arrays
 Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. In other words, return the symmetric difference of the two arrays.
@@ -10,3 +9,92 @@ Note: You can return the array with its elements in any order.
 */
 /* ========================== SOLUTION 1  ======================================
 ================================================================================ */
+function diffArray(arr1, arr2) {
+    const union = [];
+    for (let i = 0; i < arr1.length; i++) {
+        if (!union.includes(arr1[i])) {
+            union.push(arr1[i])
+        }
+    }
+    for (let i = 0; i < arr2.length; i++) {
+        if (!union.includes(arr2[i])) {
+            union.push(arr2[i])
+        }
+    }
+    const symmDiff = [];
+    for (let i = 0; i < union.length; i++) {
+        const currentEl = union[i]
+        if (arr1.includes(currentEl) && !arr2.includes(currentEl)) {
+            symmDiff.push(currentEl)
+        } else if (!arr1.includes(currentEl) && arr2.includes(currentEl)) {
+            symmDiff.push(currentEl)
+        }
+    }
+    return symmDiff;
+}
+
+console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));//should return [4].
+console.log(diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]));//should return ["pink wool"].
+console.log(diffArray(["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]));//should return ["diorite", "pink wool"].
+console.log(diffArray(["andesite", "grass", "dirt", "dead shrub"], ["andesite", "grass", "dirt", "dead shrub"])); //should return [].
+console.log(diffArray([1, "calf", 3, "piglet"], [1, "calf", 3, 4]));//should return ["piglet", 4].
+console.log(diffArray([], ["snuffleupagus", "cookie monster", "elmo"])); //should return ["snuffleupagus", "cookie monster", "elmo"].
+console.log(diffArray([1, "calf", 3, "piglet"], [7, "filly"])); //should return [1, "calf", 3, "piglet", 7, "filly"].
+
+
+/* ========================== SOLUTION 2  ======================================
+================================================================================ */
+function diffArray(arr1, arr2) {
+    const union = [];
+    for (const el of arr1) {
+        if (!union.includes(el)) {
+            union.push(el)
+        }
+    }
+    for (const el of arr2) {
+        if (!union.includes(el)) {
+            union.push(el)
+        }
+    }
+    const symmDiff = [];
+    for (const currentEl of union) {
+        if (arr1.includes(currentEl) && !arr2.includes(currentEl)) {
+            symmDiff.push(currentEl)
+        } else if (!arr1.includes(currentEl) && arr2.includes(currentEl)) {
+            symmDiff.push(currentEl)
+        }
+    }
+    return symmDiff;
+}
+
+console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));//should return [4].
+console.log(diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]));//should return ["pink wool"].
+console.log(diffArray(["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]));//should return ["diorite", "pink wool"].
+console.log(diffArray(["andesite", "grass", "dirt", "dead shrub"], ["andesite", "grass", "dirt", "dead shrub"])); //should return [].
+console.log(diffArray([1, "calf", 3, "piglet"], [1, "calf", 3, 4]));//should return ["piglet", 4].
+console.log(diffArray([], ["snuffleupagus", "cookie monster", "elmo"])); //should return ["snuffleupagus", "cookie monster", "elmo"].
+console.log(diffArray([1, "calf", 3, "piglet"], [7, "filly"])); //should return [1, "calf", 3, "piglet", 7, "filly"].
+
+/* ========================== SOLUTION 3  ======================================
+================================================================================ */
+function diffArray(arr1, arr2) {
+    const union = [...new Set([...arr1, ...arr2].flat())];
+    return union.reduce((output, curr) => {
+        if (arr1.includes(curr) && !arr2.includes(curr)) {
+            output.push(curr)
+        } else if (!arr1.includes(curr) && arr2.includes(curr)) {
+            output.push(curr)
+        }
+        return output;
+    }, [])
+}
+
+console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));//should return [4].
+console.log(diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]));//should return ["pink wool"].
+console.log(diffArray(["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]));//should return ["diorite", "pink wool"].
+console.log(diffArray(["andesite", "grass", "dirt", "dead shrub"], ["andesite", "grass", "dirt", "dead shrub"])); //should return [].
+console.log(diffArray([1, "calf", 3, "piglet"], [1, "calf", 3, 4]));//should return ["piglet", 4].
+console.log(diffArray([], ["snuffleupagus", "cookie monster", "elmo"])); //should return ["snuffleupagus", "cookie monster", "elmo"].
+console.log(diffArray([1, "calf", 3, "piglet"], [7, "filly"])); //should return [1, "calf", 3, "piglet", 7, "filly"].
+
+
--- Intermediate-Algorithm-Scripting/sum-all-numbers-in-a-range.js
+++ Intermediate-Algorithm-Scripting/sum-all-numbers-in-a-range.js
@@ -10,3 +10,64 @@ For example, sumAll([4,1]) should return 10 because sum of all the numbers betwe
 */
 /* ========================== SOLUTION 1  ======================================
 ================================================================================ */
+function sumAll(arr) {
+    let min, max;
+    if (arr[0] > arr[1]) {
+        max = arr[0]
+        min = arr[1]
+    } else {
+        max = arr[1]
+        min = arr[0]
+    }
+    let sum = 0;
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
+}
+
+console.log(sumAll([1, 4])); // 10
+
+/* ========================== SOLUTION 2  ======================================
+================================================================================ */
+function sumAll1(arr) {
+    const min = arr[0] < arr[1] ? arr[0] : arr[1]
+    const max = arr[0] > arr[1] ? arr[0] : arr[1]
+    let sum = 0;
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
+}
+
+console.log(sumAll1([4, 4])); // 4
+
+/* ========================== SOLUTION 2  ======================================
+================================================================================ */
+function sumAll2(arr) {
+    let sum = 0;
+    for (let i = Math.min(...arr); i <= Math.max(...arr); i++) {
+        sum += i;
+    }
+    return sum;
+}
+
+console.log(sumAll2([1, 4])); // 4
+/* ========================== SOLUTION 3  ======================================
+================================================================================ */
+function sumAll(arr) {
+    // check which number is max
+    const max = Math.max(...arr);
+    // check which number is min
+    const min = Math.min(...arr);
+    // declare a variable sum assign to 0;
+    let sum = 0;
+    // iterate thru max number, init first index assign to min
+    for (let i = min; i <= max; i++) {
+        // each time iteration, increment min by 1 and reassign sum to it;
+        sum += i
+    }
+    return sum;
+}
+console.log(sumAll2([1, 4])); // 4
+
